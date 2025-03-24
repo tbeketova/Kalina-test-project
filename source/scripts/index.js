74492f135371388bbc7b63396d25a7ecc0563e7c
Tatiana B <tbeketova04@gmail.com>
2025-03-24 22:01:24 +0200

    added style for review and form

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -8,3 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+const swiper = new Swiper (".mySwiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.slider-1-button-next',
+    prevEl: '.slider-1-button-prev',
+  },
+});
+
---
+++ source/scripts/modules/swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper (".mySwiper", {
+  slidesPerView: 3,
+  /* grid: {
+    rows: 2,
+  }, */
+  /* spaceBetween: 30, */
+  /* pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }, */
+  navigation: {
+    nextEl: '.slider-1-button-next',
+    prevEl: '.slider-1-button-prev',
+  },
+});
+
+
